8c94dd118eef467875704a626bbd29dbd270c276
Sergio <sergiorangelalvarez@gmail.com>
2017-07-27 08:32:17 -0400

    Añadi unos botones a la UI

--- TMesh_2.0/Form1.Designer.cs
+++ TMesh_2.0/Form1.Designer.cs
@@ -38,6 +38,10 @@
             this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ayudaToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -51,9 +55,9 @@
             this.Board.BackColor = System.Drawing.Color.Black;
             this.Board.ColorBits = ((byte)(32));
             this.Board.DepthBits = ((byte)(16));
-            this.Board.Location = new System.Drawing.Point(12, 27);
+            this.Board.Location = new System.Drawing.Point(12, 64);
             this.Board.Name = "Board";
-            this.Board.Size = new System.Drawing.Size(667, 481);
+            this.Board.Size = new System.Drawing.Size(591, 444);
             this.Board.StencilBits = ((byte)(0));
             this.Board.TabIndex = 0;
             this.Board.Paint += new System.Windows.Forms.PaintEventHandler(this.PaintGL);
@@ -121,11 +125,51 @@
             this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
             this.acercaDeToolStripMenuItem.Text = "Acerca de";
             // 
+            // button3
+            // 
+            this.button3.Image = global::TMesh_2._0.Properties.Resources.ZoomIn;
+            this.button3.Location = new System.Drawing.Point(298, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(40, 38);
+            this.button3.TabIndex = 4;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Image = global::TMesh_2._0.Properties.Resources.ZoomOut;
+            this.button4.Location = new System.Drawing.Point(344, 21);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(36, 37);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Image = global::TMesh_2._0.Properties.Resources.gift;
+            this.button2.Location = new System.Drawing.Point(216, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(40, 38);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::TMesh_2._0.Properties.Resources.Save;
+            this.button1.Location = new System.Drawing.Point(171, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(39, 38);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1020, 520);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.Board);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -150,6 +194,10 @@
         private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
